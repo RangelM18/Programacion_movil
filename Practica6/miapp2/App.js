4cb9f5adf0bd8b51df2528163e8827080dd21317
RangelM18 <122044956@upq.edu.mx>
2026-06-09 20:16:19 -0600

    Practica 6 - Parte 1
    Estructura basica de un proyecto, componente nativo y componente propio

---
+++ Practica6/miapp2/App.js
@@ -0,0 +1,33 @@
+/* Zona1 : importaciones de componenetes y archivos */
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Saludo } from './components/Saludo';
+import { Saludo, Saludo2 } from './components/Saludo2';
+
+
+/*MAIN : Donde estan en uso los componentes */
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/wave.png')}></Image>
+      {/* <Image/> ---> autocierre */}
+      <Text>Hola mundo React Native</Text>
+      <Saludo></Saludo>
+
+      <Saludo2></Saludo2>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+/* Zona3  : Zona de estilos y posicionamiento */
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
